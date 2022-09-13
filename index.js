d5e378344a5b19ede40bc7f64df79848120d34fd
20N31A0340 <113430356+20N31A0340@users.noreply.github.com>
2022-09-13 03:01:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,75 +1,68 @@
-let userForm = document.getElementById("user_form");
-var userEntries=[];
+let userData = document.getElementById('userData')
+userEntries=[]
 
-let errors=[]
-const retieveEntries = ()=>{
-    let entries = localStorage.getItem('userEntries')
-    if(entries){
-        entries=JSON.parse(entries)
-    }else{
-        entries=[]
+const fetchData = () => {
+    let x = localStorage.getItem('user-entries')
+    if (x) {
+        x=JSON.parse(x)
+    } else {
+        x = []
     }
-    return entries
-}
-const displayEntries = ()=>{
-let entries=retieveEntries()
-const tbleEntries = entries.map((entry)=>{
-const nameCell = `<td>${entry.FullName}</td>`
-const emailCell = `<td>${entry.email}</td>`
-const passwordCell = `<td>${entry.password}</td>`
-const dobCell = `<td>${entry.dob}</td>`
-const acceptTermsCell = `<td>${entry.acceptTerms}</td>`
-const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`
-return row
-
-}).join('\n')
-const table =` <table class='table-auto w-full'>
-    <tr>
-    <th>Name </th>
-    <th>Email </th>
-    <th>Password </th>
-    <th>Dob </th>
-    <th>Accepted terms? </th>
-    </tr>${tbleEntries}
-</table>`
-let details = document.getElementById('user-entries')
-details.innerHTML=table
+    return x
 }
 
-const saveUserForm = (event)=>{
-event.preventDefault();
-const FullName = document.getElementById('name').value
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-const dob = document.getElementById('dob').value
-const acceptTerms = document.getElementById('acceptTerms').checked
-var currentYear = new Date().getFullYear();
-var birthYear = dob.split("-");
-let year=birthYear[0]
-var age = currentYear-year
-console.log({age,currentYear,birthYear})
-if(age < 18 || age > 55){
-    document.getElementById('dob').style='border:1px solid red'
-  return  alert("Age must be between 18 and 55")
+let tabulateEntries = () => {
+    let fetchedData = fetchData()
+    const tableEntries = fetchedData.map((entry) => {
+        const nameitem = `<td>${entry.name}</td>`//this is called string interpolation
+        const emailitem = `<td>${entry.email}</td>`
+        const passworditem = `<td>${entry.password}</td>`
+        const dobitem = `<td>${entry.dob}</td>`
+        const termsAcceptitem = `<td>${entry.termsAccepted}</td>`
 
-}else{
-    document.getElementById('dob').style='border:none'
+        const row = `<tr>${nameitem} ${emailitem} ${passworditem} ${dobitem} ${termsAcceptitem}</tr>`
+        return row
+    }).join('\n')
 
-    const entry ={
-        FullName,
-        email,
-        password,
-        dob,
-        acceptTerms
-     }
-     userEntries=retieveEntries()
-     userEntries.push(entry);
-     localStorage.setItem("userEntries",JSON.stringify(userEntries))
-    displayEntries()
-    userForm.reset()
-   
+    const table = `<table>
+        <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Dob</th>
+        <th>Accepted terms?</th>
+        </tr>${tableEntries}
+    </table>`
+
+    document.getElementById('user-entries').innerHTML = table
 }
- 
+
+const savingData = (event) => {
+    event.preventDefault()
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const dob = document.getElementById('dob').value
+
+    const termsAccepted = document.getElementById('acceptTerms').checked
+
+    presentYear = new Date().getFullYear()
+    userYear = dob.split('-')[0]
+    userAge=presentYear-userYear
+    if(userAge<18||userAge>55){
+        document.getElementById('dob').style='border:1px solid red'
+  return  alert("Sorry! User age must be between 18 to 55")
+    }else{
+        const enteringData = {
+            name, email, password, dob, termsAccepted
+        }
+        userEntries = fetchData()
+        userEntries.push(enteringData)
+    
+        localStorage.setItem('user-entries', JSON.stringify(userEntries))
+        tabulateEntries()
+        userData.reset()
+    }
 }
-userForm.addEventListener('submit',saveUserForm)
-displayEntries()
+userData.addEventListener('submit', savingData)
+tabulateEntries()
